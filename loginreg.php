4981f9a6a21bc09b3487c6713df844e555d83335
Shannonsahdeo <165793726+Shannonsahdeo@users.noreply.github.com>
2024-10-20 22:28:43 +0200

    updated code on php

--- loginreg.php
+++ loginreg.php
@@ -1,9 +1,6 @@
-<?php
-
-
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Sign Up</title>
 		<style>
 			body {
