564abc29293d4a4dd5c5c2d83294093249a04d0d
Shannonsahdeo <165793726+Shannonsahdeo@users.noreply.github.com>
2024-11-14 12:52:37 +0200

    change something in journal

--- write_entry.php
+++ write_entry.php
@@ -130,7 +130,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </form>
 
         <!-- Back to Home button -->
-        <a href="index.php" class="back-home-btn">Back to Home</a>
+        <a href="journal.php" class="back-home-btn">Back to Home</a>
     </div>
 
 </body>
